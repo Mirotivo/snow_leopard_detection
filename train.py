182043587d89cc8fe665a0ce4b2e98a31caf641f
Amr Badr <Amr.Mostafa@live.com>
2023-06-20 22:17:57 +1000

    added negative experiments

--- train.py
+++ train.py
@@ -3,57 +3,93 @@ import subprocess
 experiments = [
 
     # ####### YOLOV3 model ###########
-    # 'balloon_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
-    # 'balloon_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
-    # 'balloon_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
-    # 'leopard_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
-    # 'leopard_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
-    # 'leopard_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
-    # 'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
-    # 'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
-    # 'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
+    'balloon_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
+    'balloon_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
+    'balloon_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
+    'leopard_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
+    'leopard_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
+    'leopard_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
+    'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
+    'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
+    'leopard_v300_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
+    'leopard_v500_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
+    'leopard_v500_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
+    'leopard_v500_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
 
 
-    # # ####### Mask RCNN model ###########
+    # ####### Mask RCNN model ###########
 
 
 
-    # # ####### RetinaNet model ###########
-    # 'balloon_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
-    # 'leopard_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
-    # 'leopard_v300_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
+    # ####### RetinaNet model ###########
+    'balloon_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
+    'leopard_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
+    'leopard_v300_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
+    'leopard_v500_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
 
-    # # ####### HTC model ###########
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
-    # 'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
-    # 'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
-    # 'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
+    # ####### HTC model ###########
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
+    'balloon_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
+    'leopard_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
+    'leopard_v300_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
+    'leopard_v500_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
 
     # ####### HTC (DetectoRS_ResNet) model ###########
-    # '',
+    'balloon_HybridTaskCascade_DetectoResNet50_SGD_ep100_lr_0.0005_step8_11',
+    'leopard_HybridTaskCascade_DetectoResNet50_SGD_ep100_lr_0.0005_step8_11',
+    'leopard_v300_HybridTaskCascade_DetectoResNet50_SGD_ep100_lr_0.0005_step8_11',
+    'leopard_v500_HybridTaskCascade_DetectoResNet50_SGD_ep100_lr_0.0005_step8_11',
+
+
+    'leopard_v500_neg_YOLOV3_MobileNetV2_SGD_lr0.01_mom0.9_wd0.0001_ep1000',
+    'leopard_v500_neg_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000',
+    'leopard_v500_neg_YOLOV3_MobileNetV2_SGD_lr0.00001_mom0.9_wd0.0001_ep1000_bs8',
+
+    'leopard_v500_neg_RetinaNet_ResNet50_SGD_lr0.02_mom0.9_wd0.0001_ep1000',
+
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep10_lr_0.01_step200_250',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep50_lr0.0001_step200_250',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_11',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step8_50',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs4',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs8',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200_bs32',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step50_200',
+    'leopard_v500_neg_HybridTaskCascade_ResNet50_SGD_ep500_lr0.0025_step150_200',
+
+    'leopard_v500_neg_HybridTaskCascade_DetectoResNet50_SGD_ep100_lr_0.0005_step8_11',
+
 ]
 
 
